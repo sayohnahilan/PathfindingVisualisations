3126e58d46a19bc48d03b8697aa7ff011af4d5ac
sayohnahilan <sayohnahilan@gmail.com>
2020-05-06 17:35:12 -0400

    useTKINTER

--- app.py
+++ app.py
@@ -1,36 +1,88 @@
-import pygame
-from nodes import node
-from startend import startEnd
 
-pygame.init()
+# The code for changing pages was derived from: http://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter
+# License: http://creativecommons.org/licenses/by-sa/3.0/
 
-size = [800, 800]
-window = pygame.display.set_mode(size)
-pygame.display.set_caption('Visualizing A Star Path Finding')
+import tkinter as tk
 
-clock = pygame.time.Clock()
-fps = 120
 
-start = startEnd(20, 20, "start")
-end = startEnd(760, 760, "end")
+class SeaofBTCapp(tk.Tk):
 
-grid = []
-for y in range(0, 800, 20):
-    for x in range(0, 800, 20):
-        grid.append(node(x, y, "nth"))
+    def __init__(self, *args, **kwargs):
 
-running = True
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.display.quit()
-            pygame.quit()
+        container.pack(side="top", fill="both", expand = True)
 
-    for box in grid:
-        box.draw(window)
-    start.draw(window)
-    end.draw(window)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-    pygame.display.update()
-    clock.tick(fps)
+        self.frames = {}
+
+        for F in (StartPage, PageOne, PageTwo):
+
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self ,parent)
+        label = tk.Label(self, text="Start Page")
+        label.pack(pady=10 ,padx=10)
+
+        button = tk.Button(self, text="Visit Page 1",
+                           command=lambda: controller.show_frame(PageOne))
+        button.pack()
+
+        button2 = tk.Button(self, text="Visit Page 2",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page One!!!")
+        label.pack(pady=10 ,padx=10)
+
+        button1 = tk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Page Two",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page Two!!!")
+        label.pack(pady=10 ,padx=10)
+
+        button1 = tk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Page One",
+                            command=lambda: controller.show_frame(PageOne))
+        button2.pack()
+
+
+
+app = SeaofBTCapp()
+app.mainloop()
