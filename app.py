80b4f551178cf7b6ab86d978c4ca2e300082d03e
sayohnahilan <sayohnahilan@gmail.com>
2020-05-06 21:08:19 -0400

    grabFromOptions

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 from screen import *
 
+# run options window
 app = mainWindow()
 app.mainloop()
+
+row = (800, 40)
+col = (800, 40)
+
+# pull data from options window
+startX = app.gameOptions["startX"].get()
+startY = app.gameOptions["startY"].get()
+endX = app.gameOptions["endX"].get()
+endY = app.gameOptions["endY"].get()
+
+print("Sayohn", startX, startY, endX, endY)
